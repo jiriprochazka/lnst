4fa4003582fb3fe459ffda590a6c7f0e5aff03fd
Jiri Prochazka <jprochaz@redhat.com>
2015-04-20 14:01:38 +0200

    Add netperf to test task

--- recipes/netem/simple_test.py
+++ recipes/netem/simple_test.py
@@ -3,8 +3,8 @@ from lnst.Controller.Task import ctl
 hostA = ctl.get_host("machine1")
 hostB = ctl.get_host("machine2")
 
-hostA.sync_resources(modules=["IcmpPing"])
-hostB.sync_resources(modules=["IcmpPing"])
+hostA.sync_resources(modules=["IcmpPing", "Netperf"])
+hostB.sync_resources(modules=["IcmpPing", "Netperf"])
 
 ping_mod = ctl.get_module("IcmpPing",
                            options={
@@ -14,4 +14,33 @@ ping_mod = ctl.get_module("IcmpPing",
                               "iface" : hostA.get_devname("testiface"),
                               "limit_rate": 90})
 
+netperf_srv = ctl.get_module("Netperf",
+                              options={
+                                  "role" : "server",
+                                  "bind" : hostA.get_ip("testiface")
+                            })
+
+netperf_cli_tcp = ctl.get_module("Netperf",
+                                  options={
+                                      "role" : "client",
+                                      "netperf_server" : hostA.get_ip("testiface"),
+                                      "duration" : 60,
+                                      "testname" : "TCP_STREAM",
+                                      "netperf_opts" : "-L %s" % hostB.get_ip("testiface")
+                                  })
+
+netperf_cli_udp= ctl.get_module("Netperf",
+                                  options={
+                                      "role" : "client",
+                                      "netperf_server" : hostA.get_ip("testiface"),
+                                      "duration" : 60,
+                                      "testname" : "UDP_STREAM",
+                                      "netperf_opts" : "-L %s" % hostB.get_ip("testiface")
+                                  })
+
 hostA.run(ping_mod)
+server_proc = hostA.run(netperf_srv, bg=True)
+ctl.wait(2)
+hostB.run(netperf_cli_tcp, timeout=70)
+hostB.run(netperf_cli_udp, timeout=70)
+server_proc.intr()
