86ffbd3e3fe513d4dbdc2729a7f3852b329f6994
Jiri Prochazka <jprochaz@redhat.com>
2015-05-12 17:14:12 +0200

    Konverze na dict z XML data v NetTestControlleru

--- lnst/Controller/NetTestController.py
+++ lnst/Controller/NetTestController.py
@@ -326,10 +326,10 @@ class NetTestController:
                 iface.set_option(opt["name"], opt["value"])
 
         if "netem" in iface_xml_data:
-            iface.set_netem(iface_xml_data["netem"])
+            iface.set_netem(iface_xml_data["netem"].to_dict())
 
         if "ovs_conf" in iface_xml_data:
-            iface.set_ovs_conf(iface_xml_data["ovs_conf"])
+            iface.set_ovs_conf(iface_xml_data["ovs_conf"].to_dict())
 
         if iface_xml_data["netns"] != None:
             iface.set_netns(iface_xml_data["netns"])
